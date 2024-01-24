5d24118146b4c106391bf77e8e8f7479d183c2bf
tejashvvce <156428021+tejashvvce@users.noreply.github.com>
2024-01-24 15:28:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+let currentSlide=0;
+
+function showSlide()
+{
+    const wrapper=document.querySelector('.slider-wrapper')
+    const slideWidth=document.querySelector('.slider-slide').clientWidth;
+    wrapper.style.transform=`translateX(${-currentSlide * slideWidth}px)`;
+}
+
+function prevSlide()
+{
+    if(currentSlide>0)
+    {
+        currentSlide--;
+    }
+    else{
+        currentSlide=1;
+    }
+    showSlide();
+}
+
+
+function nextSlide()
+{
+    if(currentSlide<1)
+    {
+        currentSlide++;
+    }
+    else{
+        currentSlide=0;
+    }
+    showSlide();
+}
